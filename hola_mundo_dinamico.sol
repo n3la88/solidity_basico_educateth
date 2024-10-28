40a6d834cd74384c3afa47d9af2b50c0fd3c0430
n3la88 <mari.montab@gmail.com>
2024-10-27 21:49:36 -0600

    OBL Parte 2A Clase 3 | Sintaxis II

--- hola_mundo_dinamico.sol
+++ hola_mundo_dinamico.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.24;
+pragma solidity ^0.8.24;
 
 contract hola_mundo_dinamico{
-    string public saludo_d = "Hola mundo dinamuico";
+    string public saludo_d = "Hola mundo dinamico";
     string public saludo_e = "Saludo inicial en el despliegue";
 
-    function leerSaludo () public view returns (string memory) {
+    function leerSaludo() public view returns (string memory) {
         return saludo_d;
     }
 
--- variables_nativas.sol
+++ variables_nativas.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.24;
+pragma solidity ^0.8.24;
 
 contract variables_nativas {
+
     string private  fullName; 
 
     constructor(string memory _fullname) {
@@ -13,5 +14,21 @@ contract variables_nativas {
         return fullName;
     }
 
+    /*
+    uint enteros sin signo
+    uint8 rango de 0 a 2 ** 8 -1
+    uint16 rango de 0 a 2 ** 16 -1
+    */
     
+    uint8 public uint_8 = 1;
+    uint16 public uint_16 = 123;
+
+    // int permite guardar numeros con signo negativo
+
+    int8 public int_8 = -1;
+
+    //address 
+
+    address public my_address = 0xd91af943a487D566aeC08569dAc91e205E335aEC;
+
 }
