05a9981cd123b3f4ce4ee295d2b7cac64c6f4f62
n3la88 <mari.montab@gmail.com>
2024-11-05 11:29:39 -0600

    OBL Parte 2A Clase 5 | Tokens No-Fungibles ERC721 - contrato actualizado

--- educateh.sol
+++ educateh.sol
@@ -5,16 +5,16 @@ pragma solidity ^0.8.22;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is ERC721, Ownable {
+contract Educateth is ERC721, Ownable {
     uint256 private _nextTokenId;
 
     constructor(address initialOwner)
-        ERC721("MyToken", "MTK")
+        ERC721("Educateth", "EDH")
         Ownable(initialOwner)
     {}
 
     function _baseURI() internal pure override returns (string memory) {
-        return "https://";
+        return "https://harlequin-obedient-coyote-909.mypinata.cloud/ipfs/QmNuuxCNjMBuNQ4cjdyJnqDr2fEL7oydEKHeqvoE83NyQU";
     }
 
     function safeMint(address to) public onlyOwner {
